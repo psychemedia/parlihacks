ca2637c0f66a4ef0bab1fe40e6b863139fd322fd
Tony Hirst <tony.hirst@gmail.com>
2018-01-23 14:10:35 +0000

    Fix index in shiny app

--- shinyapps/broadband/app.R
+++ shinyapps/broadband/app.R
@@ -16,9 +16,9 @@ la='Isle of Wight'
 fp='/home/rstudio/data/Broadband---Constituency-and-Ward-tables'
 #Alternatively, we could load it in directly from the CSV URL
 df = read_excel (paste0(fp,'.xlsx'), sheet = 'Ward Data', skip=2)
-df = df<-df[3:(dim(df)[1]-14),] #drop top two rows and last 5
-write.csv(df,paste0(fp,'csv'))
-df = read.csv (paste0(fp,'csv'),skip=1)
+df = df<-df[2:(dim(df)[1]-14),] #drop top two rows and last 5
+write.csv(df,paste0(fp,'.csv'))
+df = read.csv (paste0(fp,'.csv'),skip=1)
 
 library(rgdal)
 gdata=paste0("/home/rstudio/data/wards_by_lad/", gcode, ".json")
